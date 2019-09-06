fba7708cd1ff515dc1e0e1bda436a7b4f9e3b30a
Henré Botha <henrebotha@gmail.com>
2019-09-06 11:08:29 +0200

    Improve monitor script

--- xfce/scripts/monitor-layout.sh
+++ xfce/scripts/monitor-layout.sh
@@ -1,23 +1,26 @@
 #!/bin/sh
 
-date >> log.txt #log the date for debug purposes
-sleep 1 #give the system time to setup the connection
+# Adapted from https://fedidat.com/420-xfce-display-auto/
 
-#CHANGE THE 'DP-1' IN THE FOLLOWING LINE
+logfile='/home/hbotha/xfce-monitor-log.txt'
+
+date >> $logfile
+sleep 1 # Give the system time to setup the connection
+
 dmode="$(cat /sys/devices/platform/evdi.0/drm/card1/card1-DVI-I-1/status)"
 export DISPLAY=:0
 export XAUTHORITY=/home/hbotha/.Xauthority
 
 if [ "${dmode}" = disconnected ]; then
-    echo disconnected >> log.txt
+    echo disconnected >> $logfile
 elif [ "${dmode}" = connected ]; then
-    if xrandr -q | grep -q "1920"; then #the resolution should appear
-        #THIS IS THE XRANDR COMMAND, output piped to the log
-        /usr/bin/xrandr --output DVI-I-1-1 --mode 1920x1080 --pos 0x0 --rotate normal --output eDP-1 --primary --mode 1920x1080 --pos 0x1080 --rotate normal --output HDMI-2 --off --output HDMI-1 --off --output DP-2 --off --output DP-1 --off 2>&1 | tee -a log.txt
-        echo "success" >> log.txt
+    # We should see our desired monitor's resolution in the output
+    if xrandr -q | grep -q "1920"; then
+        /usr/bin/xrandr --output DVI-I-1-1 --mode 1920x1080 --pos 0x0 --rotate normal --output eDP-1 --primary --mode 1920x1080 --pos 0x1080 --rotate normal --output HDMI-2 --off --output HDMI-1 --off --output DP-2 --off --output DP-1 --off 2>&1 | tee -a $logfile
+        echo "success" >> $logfile
     else
-        echo "no resolution, bad setup" >> log.txt
+        echo "no resolution, bad setup" >> $logfile
     fi
 else
-    echo "unknown event" >> log.txt
+    echo "unknown event" >> $logfile
 fi
