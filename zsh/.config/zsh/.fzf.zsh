2f22fdc89d2b1a6a2fbad2b72541e8c753955615
Henré Botha <henrebotha@gmail.com>
2021-05-07 17:30:09 +0200

    Load fzf key bindings

--- zsh/.config/zsh/.fzf.zsh
+++ zsh/.config/zsh/.fzf.zsh
@@ -3,3 +3,5 @@
 if [[ ! "$PATH" == */home/hbotha/.fzf/bin* ]]; then
   export PATH="${PATH:+${PATH}:}/home/hbotha/.fzf/bin"
 fi
+
+[ -f "$HOME"/.fzf/shell/key-bindings.zsh ] && . "$HOME"/.fzf/shell/key-bindings.zsh
